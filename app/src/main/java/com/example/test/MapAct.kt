3291d3e0982efbc38587eaa00e25ad5d69e6147c
Nikita <prof.fatop@gmail.com>
2024-12-08 06:56:03 +0700

    Fixed MapAct

--- app/src/main/java/com/example/test/MapAct.kt
+++ app/src/main/java/com/example/test/MapAct.kt
@@ -1,75 +1,95 @@
 package com.example.test.map
 
 import android.content.Context
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.OvalShape
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.OvalShape
-import androidx.compose.ui.graphics.toArgb
 
 data class SignalPoint(val latitude: Double, val longitude: Double, val rsrp: Int)
 
 @Composable
 fun MapViewComposable(context: Context, latitude: Double?, longitude: Double?, rsrp: Int?) {
+    val mapView = remember { MapView(context) }
     val signalPoints = remember { mutableStateListOf<SignalPoint>() }
+    var isInitialSetup by remember { mutableStateOf(true) }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
+                mapView.onResume()
+            } else if (event == Lifecycle.Event.ON_PAUSE) {
+                mapView.onPause()
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
 
     AndroidView(
         modifier = Modifier.fillMaxSize(),
         factory = {
-            val mapView = MapView(context)
             Configuration.getInstance().userAgentValue = context.packageName
             mapView.setTileSource(TileSourceFactory.MAPNIK)
             mapView.controller.setZoom(18.0)
             mapView.isTilesScaledToDpi = true
+            mapView.controller.setCenter(GeoPoint(latitude ?: 0.0, longitude ?: 0.0)) // Установка начального положения
             mapView
         },
-        update = { mapView ->
-            if (latitude != null && longitude != null) {
-                val geoPoint = GeoPoint(latitude, longitude)
-                mapView.controller.setCenter(geoPoint)
-
-                // Обновляем позицию маркера текущего местоположения, если он есть
-                if (mapView.overlays.size > 0) {
-                    val marker = mapView.overlays[0] as Marker
-                    marker.position = geoPoint
-                } else {
-                    // Если маркера нет, создаем его
-                    val marker = Marker(mapView)
-                    marker.position = geoPoint
-                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                    mapView.overlays.add(marker)
-                }
+        update = {
+            if (isInitialSetup && latitude != null && longitude != null) {
+                mapView.controller.setCenter(GeoPoint(latitude, longitude))
+                isInitialSetup = false
             }
+
             if (latitude != null && longitude != null && rsrp != null) {
-                if (signalPoints.size >= 10) {
-                    signalPoints.removeFirst()
-                }
                 signalPoints.add(SignalPoint(latitude, longitude, rsrp))
             }
 
-            mapView.overlays.removeAll { it !is Marker || (it is Marker && it.title != "Current Location")  }
 
             signalPoints.forEach { point ->
-                val marker = Marker(mapView)
-                marker.position = GeoPoint(point.latitude, point.longitude)
-                marker.icon = mapRsrpToDrawable(context, point.rsrp)
-                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                marker.title = "Signal Strength: ${point.rsrp}"
-                mapView.overlays.add(marker)
-
+                val exists = mapView.overlays.any { it is Marker && it.title == "Signal Strength: ${point.rsrp}" }
+                if (!exists) {
+                    val marker = Marker(mapView)
+                    marker.position = GeoPoint(point.latitude, point.longitude)
+                    marker.icon = mapRsrpToDrawable(context, point.rsrp)
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                    marker.title = "Signal Strength: ${point.rsrp}" // Используем title для идентификации маркеров
+                    mapView.overlays.add(marker)
+                }
             }
+
+            if (latitude != null && longitude != null) {
+                val geoPoint = GeoPoint(latitude, longitude)
+
+                // Удаляем старый маркер местоположения, если он есть
+                mapView.overlays.removeAll { it is Marker && it.title == "Текущее Положение" }
+
+                // Создаем и добавляем маркер текущего местоположения
+                val locationMarker = Marker(mapView)
+                locationMarker.position = geoPoint
+                locationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                locationMarker.title = "Текущее Положение"
+                mapView.overlays.add(locationMarker)
+            }
+
             mapView.invalidate()
         }
     )
